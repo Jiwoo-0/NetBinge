0553b8e2253bc6041bc1bff642decf63d236f08c
Jiwoo-0 <ropeta.aaron@gmail.com>
2025-10-06 17:25:41 +0800

    added "popular this week" section

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./Navbar";
-import { Bookmark, Play } from "lucide-react";
+import { Bookmark, Play, Film } from "lucide-react";
 
 const Homepage = () => {
     return(
@@ -20,14 +20,25 @@ const Homepage = () => {
                     <div className="flex space-x-5">
                         <a href="#" className="flex items-center py-2 px-6 bg-red-800 rounded-md hover:bg-red-600 transition"><Play className="w-5 h-5 mr-2"/>Play</a>
                         <a href="#" className="flex items-center py-2 px-4 border text-white rounded-md hover:bg-white hover:text-black transition"><Bookmark className="w-5 h-5 mr-2"/>Add Watchlist</a>
-                        <a href="#" className="flex items-center py-2 px-4 border text-white rounded-md hover:bg-white hover:text-black transition"><Bookmark className="w-5 h-5 mr-2"/>Add Watchlist</a>
                     </div>
                 </div>
             </div>
         </section>
         <section>
-            <div className="w-full h-[80vh]">
-
+            <div className="w-full h-[80vh] px-20">
+                <div className="flex flex-col h-[40vh] p-5 space-y-5">
+                    <div className="items-center justify-center">
+                        <h1 className="text-3xl font-bold">Popular this week</h1>
+                    </div>
+                    <div className="flex flex-1 p-3 space-x-5">
+                        <div className="flex items-center w-fit">
+                            <h1 className="mt-auto text-9xl font-black">1</h1>
+                            <div className="bg-gray-400 rounded-xl w-40 h-56 overflow-hidden flex-shrink-0">
+                                <img src="/peacemakerposter.jpeg" alt="" className="w-full h-full object-cover" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
         </>
